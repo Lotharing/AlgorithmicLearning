dd091a06dccdedb324e168ee71a7a1df064bda60
Lotharing <809676295@qq.com>
2019-09-10 14:27:22 +0800

    单链表（单指针单向）

---
+++ src/top/lothar/interview/SinglyList.java
@@ -0,0 +1,27 @@
+package top.lothar.interview;
+/**
+ * 单链表结构
+ * @author 赵路通
+ *
+ */
+public class SinglyList {
+	
+	private int data;
+	
+	SinglyList next;
+	
+	public SinglyList(int data) {
+		// TODO Auto-generated constructor stub
+		this.data=data;
+		this.next=null;
+	}
+	
+	
+	public int getData() {
+		return data;
+	}
+	
+	public void setData(int data) {
+		this.data=data;
+	}
+}
